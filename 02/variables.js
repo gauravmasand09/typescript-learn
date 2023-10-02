1e57075c47839b4d9490e7c65f48b165c9e8d2c5
Gaurav Singh <gaurav.masand09@gmail.com>
2023-10-03 00:17:29 +0100

    02

---
+++ 02/variables.js
@@ -0,0 +1,4 @@
+// Typescript is Strongly TYpes Language
+var found = true;
+found = false;
+console.log("found", found);
---
+++ 02/variables.ts
@@ -0,0 +1,7 @@
+// Typescript is Strongly TYpes Language
+
+let found:boolean = true;
+found = false;
+console.log("found",found)
+
+found = 23.4 // compile time error
