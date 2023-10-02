8ef0f244ee372990fe0bdb6dcec8433f3822da1a
Gaurav Singh <gaurav.masand09@gmail.com>
2023-10-03 00:47:50 +0100

    Variables and For loops and arrays added

---
+++ 03-Loops and Arrays/for-of.js
@@ -0,0 +1,9 @@
+var sports = ["Football", "Cricket", "Kho-Kho"];
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var item = sports_1[_i];
+    if (item == "Football") {
+        console.log(item + " >>>>> Favourite");
+    }
+    else
+        console.log(item);
+}
---
+++ 03-Loops and Arrays/for-of.ts
@@ -0,0 +1,7 @@
+let sports:string[] = ["Football","Cricket","Kho-Kho"];
+for (let item of sports) {
+    if(item == "Football"){
+        console.log(item + " >>>>> Favourite")
+    } else
+    console.log(item)
+ }
